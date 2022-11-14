e251101e513bf27ccf383a3407b961aa2ec245f4
okv <okvokvspb@hotmail.com>
2022-11-14 22:03:50 +0300

    14.11.22

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,31 @@
 //Определение констант
-const editBtnElt = document.querySelector('.profile__edit-button');
+const editBtnElt = document.querySelector(".profile__edit-button");
 
-const pupElt = document.querySelector('.popup');
-const pupOpndElt = document.querySelector('.popup_opened');
+const pupElt = document.querySelector(".popup");
+const pupOpndElt = document.querySelector(".popup_opened");
 
-const formElt = document.querySelector('.form');
+const formElt = document.querySelector(".form");
 
-const closeBtnElt = formElt.querySelector('.form__close-button');
-const submitBtnElt = formElt.querySelector('.form__submit-button');
+const closeBtnElt = formElt.querySelector(".form__close-button");
+const submitBtnElt = formElt.querySelector(".form__submit-button");
 
-let nameElt = document.querySelector('.profile__title')
-let descriptionElt = document.querySelector('.profile__subtitle')
+let nameElt = document.querySelector(".profile__title")
+let descriptionElt = document.querySelector(".profile__subtitle")
 
 let fieldTop = formElt.querySelector(".form__field-name");
 let fieldBtm = formElt.querySelector(".form__field-description");
 
 //Определение функци добавления класса для открытия окна
 let pupOpened = function () {
-	pupElt.classList.add('popup_opened');
+	pupElt.classList.add("popup_opened");
 	//задание значений полей Name и Description с помощью value
 	fieldTop.value = nameElt.textContent;
 	fieldBtm.value = descriptionElt.textContent;
-	console.log(fieldTop.textContent);
-};
+	};
 
 //Определение функции удаления класса для закрытия окна
 let pupClosed = function () {
-	pupElt.classList.remove('popup_opened');
+	pupElt.classList.remove("popup_opened");
 };
 
 //Определение функции отправки
@@ -38,9 +37,9 @@ let formSubmit = function () {
 };
 
 //Проверка события нажатия на кнопку редактировать профиль и запуск ф-ии
-editBtnElt.addEventListener('click', pupOpened), { once: true };
+editBtnElt.addEventListener("click", pupOpened), { once: true };
 //Если форма открыта, отключить отслеживание нажатия на кнопку Редактировать, отслеживать событие нажатия на кнопку Сохранить или на кнопку Закрыть
 //editBtnElt.removeEventListener();
-closeBtnElt.addEventListener('click', pupClosed);
-submitBtnElt.addEventListener('click', formSubmit);
+closeBtnElt.addEventListener("click", pupClosed);
+submitBtnElt.addEventListener("submit", formSubmit);
 
