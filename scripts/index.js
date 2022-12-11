e1d53efe05f478e549263b7cebcd1c8b7d748e0c
okv <okvokvspb@hotmail.com>
2022-12-11 23:04:15 +0300

    11.12.2022 испр.

--- scripts/index.js
+++ scripts/index.js
@@ -7,17 +7,17 @@ const addBtnElt = profileElt.querySelector('.profile__add-button');
 const cardsGrid = document.querySelector('.elements__grid');
 
 //всплывающие окна
-const pupNodeList = document.querySelectorAll('.popup')
-const pupEdit = document.querySelector('.popup_type_edit');
-const pupAdd = document.querySelector('.popup_type_add');
-const pupImg = document.querySelector('.popup_type_img');
+const popups = document.querySelectorAll('.popup')
+const popupEditProfile = document.querySelector('.popup_type_edit');
+const popupAddCard = document.querySelector('.popup_type_add');
+const popupImage = document.querySelector('.popup_type_img');
 
-const pupImgElt = pupImg.querySelector('.popup__image');
-const pupImgTextElt = pupImg.querySelector('.popup__image-caption')
+const imagePopup = popupImage.querySelector('.popup__image');
+const imageText = popupImage.querySelector('.popup__image-caption')
 
 //получение значений для заполнения формы редактирования профиля
-const prflNameElt = profileElt.querySelector('.profile__title');
-const prflDescriptionElt = profileElt.querySelector('.profile__subtitle');
+const userName = profileElt.querySelector('.profile__title');
+const userDescription = profileElt.querySelector('.profile__subtitle');
 
 const cardTemplate = document.querySelector('.cardTemplate').content;
 
@@ -47,12 +47,12 @@ function createNewCard(cardTextElt, cardLinkElt) {
 	//присоединение проверки нажатия на картинку
 	newImgElt.addEventListener('click', (event) => {
 		//заполнение
-		pupImgElt.src = event.target.src;
-		pupImgElt.alt = event.target.alt;
+		imagePopup.src = event.target.src;
+		imagePopup.alt = event.target.alt;
 		const elementElt = event.target.closest('.element')
-		const imgTextElt = elementElt.querySelector('.element__text');
-		pupImgTextElt.textContent = imgTextElt.textContent;
-		openPopup(pupImg);
+		const elementText = elementElt.querySelector('.element__text');
+		imageText.textContent = elementText.textContent;
+		openPopup(popupImage);
 	});
 	return newCard;
 };
@@ -65,54 +65,51 @@ function addCard(cardTextElt, cardLinkElt) {
 
 //-------------------------------------------------------------------------
 //Форма редактирования профиля
-const editFormElt = document.forms.editForm;
-const nameElt = editFormElt.elements.first;
-const descriptionElt = editFormElt.elements.second;
-const submitBtnEdElt = editFormElt.querySelector('.form__submit-button');
+const formEditProfile = document.forms.editForm;
+const inputName = formEditProfile.querySelector('.form__field_type_name');
+const inputDescription = formEditProfile.querySelector('.form__field_type_description');
 
 //Форма добавления контента 
-const addFormElt = document.forms.addForm;
-const textElt = addFormElt.elements.third;
-const linkElt = addFormElt.elements.fourth;
-const submitBtnAdElt = addFormElt.querySelector('.form__submit-button');
+const formAddCard = document.forms.addForm;
+const inputCardName = formAddCard.querySelector('.form__field_type_cardname');
+const inputCardLink = formAddCard.querySelector('.form__field_type_cardlink');
 
 //-----------------------------------------------------------------------
 //Функция реакции на нажатие <Esc>
-function checkEscState(event, popupType) {
+function checkEscState(event) {
 	if (event.key === 'Escape') {
+		popupType = document.querySelector('.popup_opened');
 		shutPopup(popupType);
 	};
 };
 
-function controlKey() {};
-//------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 //Функция открытия всплывающего окна 
 function openPopup(popupType) {
 	popupType.classList.add('popup_opened');
 	//проверка нажатия на кнопку <Esc>
-	const controlKey = (event) => checkEscState(event, popupType);
-	document.addEventListener('keydown', controlKey);
+	document.addEventListener('keydown', checkEscState);
 };
 
 //Функция закрытия всплывающего окна
 function shutPopup(popupType) {
 	popupType.classList.remove('popup_opened');
 	//снятие слушателя
-	document.removeEventListener('keydown', controlKey);
+	document.removeEventListener('keydown', checkEscState);
 };
 
 //------------------------------------------------------------------------------
 //Проверка нажатия на кнопку редактировать профиль 
 editBtnElt.addEventListener('click', (event) => {
 	//автозаполнение полей формы из профиля 
-	nameElt.value = prflNameElt.textContent;
-	descriptionElt.value = prflDescriptionElt.textContent;
-	openPopup(pupEdit);
+	inputName.value = userName.textContent;
+	inputDescription.value = userDescription.textContent;
+	openPopup(popupEditProfile);
 });
 
 //Проверка нажатия на кнопку добавления контента
 addBtnElt.addEventListener('click', (event) => {
-	openPopup(pupAdd);
+	openPopup(popupAddCard);
 });
 
 //-----------------------------------------------------------------------------
@@ -120,35 +117,35 @@ addBtnElt.addEventListener('click', (event) => {
 //функция отправки содержания формы редактирования профиля (event eq. submit)
 function submitEditForm(event) {
 	event.preventDefault();
-	// замена значений 
-	prflNameElt.textContent = nameElt.value;
-	prflDescriptionElt.textContent = descriptionElt.value;
-	shutPopup(pupEdit);
+	//замена значений 
+	userName.textContent = inputName.value;
+	userDescription.textContent = inputDescription.value;
+	shutPopup(popupEditProfile);
 };
 
 //функция отправки содержания формы добавления контента
 function submitAddForm(event) {
 	event.preventDefault();
 	//добавить карточку
-	addCard(textElt.value, linkElt.value);
+	addCard(inputCardName.value, inputCardLink.value);
 	//сброс полей
 	event.target.reset();
-	shutPopup(pupAdd);
+	shutPopup(popupAddCard);
 };
 
 //----------------------------------------------------------------------------
 //проверка нажатия на кнопку <Закрыть> и внешнюю часть всплывающего окна
-pupNodeList.forEach((popupType, index) => {
+popups.forEach((popupType, index) => {
 	popupType.addEventListener('click', (event) => {
-		if ((event.target === event.currentTarget) || (event.target === popupType.querySelector('.popup__close-button')) || (event.target === pupImgElt)) {
+		if ((event.target === event.currentTarget) || (event.target === popupType.querySelector('.popup__close-button'))) {
 			shutPopup(popupType);
 		};
 	});
 });
 
 //проверка нажатия на кнопку <Сохранить>
-editFormElt.addEventListener('submit', submitEditForm);
-addFormElt.addEventListener('submit', submitAddForm);
+formEditProfile.addEventListener('submit', submitEditForm);
+formAddCard.addEventListener('submit', submitAddForm);
 
 //-----------------------------------------------------------------------------
 //Функция добавления карточек из массива
